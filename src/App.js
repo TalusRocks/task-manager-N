69c9be9a7a824afd3d68324649e3d9bf8c16c24b
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-08-30 15:28:35 -0700

    start new react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="task-container">
+        <div className="task-title"></div>
+        <div className="task-note"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
