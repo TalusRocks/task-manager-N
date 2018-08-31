78c39bb06d66f6e990670da6897ce17d37af4d59
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-08-31 15:31:59 -0700

    add basic styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,34 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="task-container">
-        <div className="task-title">Title</div>
-        <div className="task-note">Note</div>
+      <div className="all-tasks-container">
+        <div className="task-container">
+          <div className="task-title"><h2>OMNI-1234</h2></div>
+          <div className="task-note todo-color">
+            <i class="material-icons todo-icon">arrow_right_alt</i>
+            <p>Note</p>
+          </div>
+          <div className="task-note todo-color">
+            <i class="material-icons todo-icon">arrow_right_alt</i>
+            <p>Note</p>
+          </div>
+          <div className="task-note todo-color">
+            <i class="material-icons todo-icon">arrow_right_alt</i>
+            <p>Note</p>
+          </div>
+          <div className="task-note question-color">
+            <i class="material-icons question-icon">help_outline</i>
+            <p>Note</p>
+          </div>
+          <div className="task-note blocked-color">
+            <i class="material-icons blocked-icon">block</i>
+            <p>Note</p>
+          </div>
+          <div className="task-note">
+            <i class="material-icons">add</i>
+            <p>Note</p>
+          </div>
+        </div>
       </div>
     );
   }
