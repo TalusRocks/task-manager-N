4b5267d6f1394cdb937fe8bbf4302d5ac9c7bb04
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-08-31 10:31:30 -0700

    add scss file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="task-container">
-        <div className="task-title"></div>
-        <div className="task-note"></div>
+        <div className="task-title">Title</div>
+        <div className="task-note">Note</div>
       </div>
     );
   }
