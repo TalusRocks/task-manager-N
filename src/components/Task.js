e35922f464b67d419300e06958361482b89dcb93
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-09-25 12:12:15 -0700

    update add button and task styling

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,8 +6,8 @@ const Task = props => {
     <div className="task-container">
       <div className="task-title"><h2>{props.task.title}</h2></div>
       <NoteList notes={props.task.notes}/>
-      <div className="task-note add-note">
-        <i className="material-icons">add</i>
+      <div className="task-note">
+        <i className="material-icons btn-add">add</i>
         <input placeholder="Add note"></input>
       </div>
     </div>
